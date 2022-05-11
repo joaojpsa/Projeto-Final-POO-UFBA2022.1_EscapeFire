376be3972cdf3d60206f29ace538a0c1541199c2
WandersonSantosCaelest <101937462+WandersonSantosCaelest@users.noreply.github.com>
2022-05-11 19:03:22 -0300

    Update Vulture.java
    Apenas comentários

--- Vulture.java
+++ Vulture.java
@@ -1,8 +1,8 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class vulture here.
- * 
+ * A classe Vulture define os objetos antagonistas
+ * Do game 
  * @author (your name) 
  * @version (a version number or a date)
  */
@@ -17,8 +17,10 @@ public class Vulture extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act(){
+       //Setlocation recebe o x e o y pelo get modificando o x pra menos 1
         setLocation(getX() -1, getY());
         if(getX() <= 15){
+            //Se o getx for menor ou igual a 15 destruir o objeto vulture
             ((Florest) getWorld()).removeObject(this);
         }
     }
