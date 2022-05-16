5e2da9b7f64e906839c2b78a8a821c455d6bafdd
Joao Abreu <joaojpsa@gmail.com>
2022-05-16 17:56:49 -0300

    Update Parrot.java

--- Parrot.java
+++ Parrot.java
@@ -14,12 +14,6 @@ public class Parrot extends Actor
     }
     private int up = 7;
     private int velDown = 1;
-    private int score;
-    
-    //private boolean isburned = false;
-    //private boolean isvulturetouch = false;
-    private boolean istouch = false;
-    
     
     /**
      * Act - do whatever the parrot wants to do. This method is called whenever
@@ -29,22 +23,9 @@ public class Parrot extends Actor
     {
         fall();
         isTouch();
-        //isBurned();
-        //isVultureTouch();
         moveParrot();
-        /*if(Greenfoot.mousePressed(null)){
-            MouseInfo mouse = Greenfoot.getMouseInfo();
-            int mouseX = mouse.getX();
-            int myX = this.getX();
-            if(mouseX > myX){
-                move(+15);
-            }
-            if(mouseX < myX){
-                move(-15);
-            }
-        }*/
+        
     }
-   
     
     private void moveParrot(){
         if(Greenfoot.isKeyDown("right")){
@@ -69,23 +50,9 @@ public class Parrot extends Actor
         setLocation(getX(), getY() - up);
     }
     
-    /*public void isBurned(){
-        isburned = true;
-        if(isTouching(Fire.class)){
-            Greenfoot.playSound("sounds/death.wav");
-            ((Florest) getWorld()).gameOver();
-        }
-    }
-    
-    public void isVultureTouch(){
-        isvulturetouch = true;
-        if(isTouching(Vulture.class)){
-            Greenfoot.playSound("sounds/death.wav");
-            ((Florest) getWorld()).gameOver();
-        }
-    }*/
+ 
     public void isTouch(){
-        istouch = true;
+        
         if(isTouching(Fire.class)){
             Greenfoot.playSound("sounds/death.wav");
             ((Florest) getWorld()).gameOver();
