619db1ea5a1c9a801f6726083bf705e4da5d95b1
joaojpsa <joaojpsa@gmail.com>
2022-05-29 11:53:45 -0300

    add

--- AllObjects.java
+++ AllObjects.java
@@ -1,27 +1,31 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class AllObjects here.
  * 
- * @author (your name) 
+ * @author (João Paulo S. Abreu (joaoabreu@ufba.br), Mateus C. Moura
+ *         (mateuschaves@ufba.br), Rafael P. Casaes Sampaio
+ *         (rafael.casaes@ufba.br),
+ *         Uanderson S. Celestino (uandersoncelestino@ufba.br), Lílian T. de
+ *         Sousa (lilian.sousa@ufba.br), Jefferson Aimon de B. Silva
+ *         (jefferson.raimon@ufba.br))
  * @version (a version number or a date)
  */
-public class AllObjects extends Actor
-{
+public class AllObjects extends Actor {
     int horScale;
     int vertScale;
+
     /**
      * Act - do whatever the AllObjects wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
+    public void act() {
         // Add your action code here.
     }
-    
-    public void scaleImage(int x,int y){
+
+    public void scaleImage(int x, int y) {
         horScale = x;
         vertScale = y;
-        getImage().scale(getImage().getWidth()/horScale, getImage().getHeight()/vertScale);
+        getImage().scale(getImage().getWidth() / horScale, getImage().getHeight() / vertScale);
     }
 }
--- Florest.java
+++ Florest.java
@@ -3,7 +3,13 @@ import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name)  * @version (a version number or a date)
+ * @author (João Paulo S. Abreu (joaoabreu@ufba.br), Mateus C. Moura
+ *         (mateuschaves@ufba.br), Rafael P. Casaes Sampaio
+ *         (rafael.casaes@ufba.br),
+ *         Uanderson S. Celestino (uandersoncelestino@ufba.br), Lílian T. de
+ *         Sousa (lilian.sousa@ufba.br), Jefferson Aimon de B. Silva
+ *         (jefferson.raimon@ufba.br))
+ *         * @version (a version number or a date)
  */
 public class Florest extends World {
     /**
@@ -12,25 +18,23 @@ public class Florest extends World {
      */
     public Florest() {
         // Create a new world with 700x500 cells with a cell size of 1x1 pixels.
-        //classe construtora de Florest
+        // classe construtora de Florest
         super(700, 500, 1);
         addObject(new Score(), 25, 476);
         addObject(new Parrot(), 151, 196);
         setBackground("images/Fdia.png");
         playloop();
-        
-        
 
     }
 
     public void act() {
-        // metodo atualiza as açoes do jogo, instanciando as classes       
+        // metodo atualiza as açoes do jogo, instanciando as classes
         if (Greenfoot.getRandomNumber(500) < 1) {
             addObject(new Cloud(), 536, Greenfoot.getRandomNumber(91));
             addObject(new Fire(), 637, 331);
             addObject(new Vulture(), 570, 15);
             addObject(new Mushroom(), 470, 47);
-                       
+
         }
         if (Greenfoot.getRandomNumber(600) < 1) {
             addObject(new Fire(), 606, 445);
@@ -47,17 +51,17 @@ public class Florest extends World {
         }
 
     }
-    
-    public void playloop(){
+
+    public void playloop() {
         Greenfoot.playSound("sounds/theme.mp3");
     }
 
     public void gameOver() {
-        //metodo pra chamar a classe GameOver
+        // metodo pra chamar a classe GameOver
         addObject(new GameOver("Game Over!"), getWidth() / 2, getHeight() / 2);
         Greenfoot.playSound("sounds/end.wav");
         Greenfoot.stop();
-        
+
     }
-       
+
 }
